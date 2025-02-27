3bbd9674c51fdf4825f769641a437ad6bb923c0c
MatheoM35 <matheomousse004@gmail.com>
2025-02-27 19:19:29 +0100

    Rename PDO variable for database connection to improve clarity

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=reservation_system;charset=utf8", "root", ""); // Crée une nouvelle instance de PDO pour se connecter à la base de données
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Définit le mode d'erreur de PDO pour lancer des exceptions
+    $bdd = new PDO("mysql:host=localhost;dbname=reservation_system;charset=utf8", "root", ""); // Crée une nouvelle instance de PDO pour se connecter à la base de données
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Définit le mode d'erreur de PDO pour lancer des exceptions
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage()); // Affiche un message d'erreur et arrête le script en cas d'exception PDO
 }
