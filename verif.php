d6fbf24706ee45ad635367ad133b9163cf36b2b4
MatheoM35 <matheomousse004@gmail.com>
2025-02-27 23:55:36 +0100

    Add SQL dump for reservation system and implement user verification check

--- verif.php
+++ verif.php
@@ -2,3 +2,15 @@
 session_start();
 require 'config.php'; // Fichier de connexion à la base de données
 
+if (isset($_GET['id']) && isset($_GET['cle']) && !empty($_GET['id']) && !empty($_GET['cle'])) {
+    
+    $getid = $_GET['id'];
+    $getcle = $_GET['cle'];
+    // $recupUser = 
+
+
+} else {
+    echo 'aucun utilisateur trouvé';
+}
+?>
+
