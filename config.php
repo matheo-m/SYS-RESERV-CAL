6c9210e64c2ebc47f462f82624eb5d9d3357b586
MatheoM35 <184085566+MatheoM35@users.noreply.github.com>
2025-02-28 09:39:32 +0100

    Update database connection settings to use port 3308

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=reservation_system;charset=utf8", "root", ""); // Crée une nouvelle instance de PDO pour se connecter à la base de données
+    $bdd = new PDO("mysql:host=localhost;port=3308;dbname=reservation_system;charset=utf8", "root", ""); // Crée une nouvelle instance de PDO pour se connecter à la base de données
+
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Définit le mode d'erreur de PDO pour lancer des exceptions
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage()); // Affiche un message d'erreur et arrête le script en cas d'exception PDO
