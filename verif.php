3db943c22608d0dbeec47563ca62d9fa1ba91a94
MatheoM35 <184085566+MatheoM35@users.noreply.github.com>
2025-02-28 09:39:11 +0100

    Implement user confirmation process in verification script

--- verif.php
+++ verif.php
@@ -6,7 +6,22 @@ if (isset($_GET['id']) && isset($_GET['cle']) && !empty($_GET['id']) && !empty($
     
     $getid = $_GET['id'];
     $getcle = $_GET['cle'];
-    // $recupUser = 
+    $recupUser = $bdd->prepare('SELECT * FROM utilisateurs WHERE id = ? AND cle = ?');
+    $recupUser->execute(array($getid, $getcle));
+    if ($recupUser->rowCount() > 0) {
+        $user_info = $recupUser->fetch();
+        if ($user_info['confirme'] == 0) {
+            $updateConfirmation = $bdd->prepare('UPDATE utilisateurs SET confirme = 1 WHERE id = ? AND cle = ?');
+            $updateConfirmation->execute(array(1, $getid));
+            $_SESSION['cle'] = $getcle;
+            header('Location: index.php');
+        } else {
+            $_SESSION['cle'] = $getcle;
+            header('Location: index.php');
+        }
+    } else {
+        echo 'Votre clé ou identifiant est invalide';
+    } 
 
 
 } else {
